873185adda630f50fc2942f708352bc20b94e4b5
jackandrews <jack@andrewsmail.co.uk>
2014-12-27 17:22:09 +0000

    commented how to use test_pipelight.py

--- plugin.video.btsportxbmc/resources/lib/test_pipelight.py
+++ plugin.video.btsportxbmc/resources/lib/test_pipelight.py
@@ -2,6 +2,10 @@ import os
 import subprocess
 from btsport_login import BtSportLogin
 
+# Enter your username and password here and then run the script. BT Sport 2 should display in fullscreen.
+USERNAME = 'my_bt_sport_username'
+PASSWORD = 'my_password'
+
 if __name__ == "__main__":
     home = os.getenv("HOME")
     cmd = os.path.abspath("%s/.xbmc/addons/plugin.video.btsportxbmc/resources/lib/pipelight.py" % home)
@@ -10,7 +14,7 @@ if __name__ == "__main__":
             "/usr/lib/mozilla/plugins", "true", "http://sport.bt.com/btsportplayer/bt-sport-2-01363810201819"]
 
     print "logging in"
-    login = BtSportLogin("", "")
+    login = BtSportLogin(USERNAME, PASSWORD)
     cookies = login.get_cookies()
     for cookie in cookies:
         args.append(cookie[0])
